caccd4ee0a8452d3ee58ea0f9f0ec34f71dfe684
Ottaliam <ottaliam@gmail.com>
2024-12-09 14:44:37 -0500

    Add documentation

--- src/components/PromptPopupButton.jsx
+++ src/components/PromptPopupButton.jsx
@@ -5,18 +5,44 @@ import { TbPrompt } from "react-icons/tb";
 import styles from "./PromptPopupButton.module.css";
 import PromptManager from "./PromptManager.jsx";
 
+/**
+ * `PromptPopupButton` is a React functional component that renders a button with a popover.
+ * The popover contains the `PromptManager` component.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered `PromptPopupButton` component.
+ */
 const PromptPopupButton = () => {
   const [anchorEl, setAnchorEl] = useState(null);
 
+  /**
+   * Event handler for opening the popover.
+   * It sets the clicked element as the anchor element for the popover.
+   *
+   * @param {Object} event - The click event object.
+   */
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
+  /**
+   * Event handler for closing the popover.
+   * It sets the anchor element to null.
+   */
   const handleClose = () => {
     setAnchorEl(null);
   };
 
+  /**
+   * @const {boolean}
+   * Controls whether the popover is open or not
+   */
   const open = Boolean(anchorEl);
+  /**
+   * @const {(string)}
+   * The id of the opened popover
+   * If the popover is not open, set to `undefined`
+   */
   const id = open ? 'simple-popover' : undefined;
 
   return (
--- src/content.jsx
+++ src/content.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import PromptPopupButton from "./components/PromptPopupButton.jsx";
 
+/**
+ * @const {function}
+ * Appends a new root DOM element with a React component to the specified target element.
+ * If a target element with the class '.gap-x-1' is found, it creates a new div element
+ * with the id 'crx-root', appends it to the target, and renders the `PromptPopupButton`
+ * React component inside it. Once the element is appended, the MutationObserver is disconnected.
+ */
 const appendRootToTarget = () => {
   const targetElement = document.querySelector('.gap-x-1');
 
@@ -20,6 +27,12 @@ const appendRootToTarget = () => {
   }
 }
 
+/**
+ * @const {MutationObserver}
+ * Mutation Observer to monitor changes in the DOM.
+ * Calls `appendRootToTarget` when an element with class '.gap-x-1' is added,
+ * and a root element with id '#crx-root' does not exist.
+ */
 const observer = new MutationObserver(mutations => {
   mutations.forEach(() => {
     if (document.querySelector('.gap-x-1') && !document.querySelector('#crx-root')) {
@@ -28,6 +41,7 @@ const observer = new MutationObserver(mutations => {
   });
 });
 
+// Start observing the document body for added or removed child elements and subtree modifications.
 observer.observe(document.body, {
   childList: true,
   subtree: true
